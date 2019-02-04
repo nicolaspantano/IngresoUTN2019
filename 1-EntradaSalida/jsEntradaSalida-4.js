4e3dd7003c3370b331d3617b69c58f1ba7d8de32
nicolaspantano <47321352+nicolaspantano@users.noreply.github.com>
2019-02-04 12:26:33 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,9 @@
 	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
 */
 function Mostrar()
-{
+{var nombre;
+	nombre=prompt("Ingrese su nombre");
+	document.getElementById("elNombre").value=nombre;
 	
 }
 
